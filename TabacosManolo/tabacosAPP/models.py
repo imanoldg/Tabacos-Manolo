1140b68092668db4ee4810b7d9ac72c8462510e3
imanoldg <ima.dominguez@opendeusto.es>
2024-10-30 14:32:20 +0100

    datos añadidos
    migraciones hechas y distribuidores, estancos y clientes creados

--- TabacosManolo/tabacosAPP/models.py
+++ TabacosManolo/tabacosAPP/models.py
@@ -7,6 +7,10 @@ class Distribuidor(models.Model):
     pais = models.CharField(max_length=100)
     exportacion = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.nombre
+
+
     
 
 
