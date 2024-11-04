976cfd8c94bde3b337da055b0009b50075e5f46a
Mikel Gonzalez <mikel.g.matos@opendeusto.es>
2024-11-04 16:20:36 +0100

    creacion de  listas y listas de detalles

--- TabacosManolo/tabacosAPP/views.py
+++ TabacosManolo/tabacosAPP/views.py
@@ -3,22 +3,59 @@ from django.http import HttpResponse, HttpResponseNotFound
 from .models import Distribuidor, Estanco, Cliente
 # Create your views here.
 def index(request):
-    return HttpResponse('primera vista')
+    return HttpResponse("Bienvenido a la aplicacion de TabacosManolo")
 
+def listaDistribuidores(request):
+            distribuidores = Distribuidor.objects.order_by('nombre')
+            nombres_distribuidores = ', '.join([distribuidor.nombre for distribuidor in distribuidores])
+            return HttpResponse(nombres_distribuidores)
+        
+def listaEstancos(request):
+            estancos = Estanco.objects.order_by('nombre')
+            nombres_estancos = ', '.join([estanco.nombre for estanco in estancos])
+            return HttpResponse(nombres_estancos)
 
-def listaDis(request):
-            empresas = Empresa.objects.order_by('nombre')
-            nombres_empresas = ', '.join([empresa.nombre for empresa in empresas])
-            return HttpResponse(nombres_empresas)
+def listaClientes(request):
+            clientes = Cliente.objects.order_by('nombre')
+            nombres_clientes = ', '.join([cliente.nombre for cliente in clientes])
+            return HttpResponse(nombres_clientes)
+
+def detalleDistribuidores(request, id_distribuidor):
+            try:
+                distribuidor = Distribuidor.objects.get(pk=id_distribuidor)
+
+                cadenaDeTexto = f"Nombre: {distribuidor.nombre}\n - Pais: {distribuidor.pais}\n - Exportacion: {distribuidor.exportacion}\n"
+
+                if distribuidor.estanco.exists():
+                    cadenaDeTexto += f"Estancos:{distribuidor.estanco}\n"
+                else:
+                    cadenaDeTexto += "No hay estancos asociados a este distribuidor"
+
+                return HttpResponse(cadenaDeTexto)
+            except Distribuidor.DoesNotExist:
+                return HttpResponseNotFound("Distribuidor no encontrado")
+            
+def detalleEstancos(request, id_estanco):
+            try:
+                estanco = Estanco.objects.get(pk=id_estanco)
+
+                cadenaDeTexto = f"Nombre: {estanco.nombre}\n - Localizacion:{estanco.localizacion}\n"
+
+                if estanco.clientes.exists():
+                    cadenaDeTexto += f"Clientes:{estanco.clientes}\n"
+                else:
+                    cadenaDeTexto += "No hay clientes asociados a este estanco"
+
+                return HttpResponse(cadenaDeTexto)
+            except Estanco.DoesNotExist:
+                return HttpResponseNotFound("Estanco no encontrado")
 
 def detalleClientes(request, id_cliente):
             try:
                 cliente = Cliente.objects.get(pk=id_cliente)
 
-                # Crear la cadena de texto con los detalles de la empresa
                 cadenaDeTexto = f"DNI: {cliente.dni}\n - Nombre:{cliente.nombre}\n - Apellido: {cliente.apellido}\n - Edad: {cliente.edad}"
 
-                # Listar los trabajadores
                 if cliente.estanco.exists():
                     cadenaDeTexto += f"Estanco:{cliente.estanco}\n"
                 else:
