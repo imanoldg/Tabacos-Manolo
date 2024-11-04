6e9e1d06dd82134a64292965a756744a1949933d
CarlosGCD <145966685+CarlosGCD@users.noreply.github.com>
2024-11-04 20:58:46 +0100

    inicio plantillas

--- TabacosManolo/tabacosAPP/urls.py
+++ TabacosManolo/tabacosAPP/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'listaE'),
+
+    path('listadoClientesConPlantillas/', views.listaClientes, name = 'listaClientesPlantillas'), 
+    path('DetalleClientesConPlantillas/', views.detalleClientes, name = 'detalleClientesPlantillas'),
+
+    path('listadoDistribuidoresConPlantillas/', views.listaDistribuidores, name = 'listaDistribuidoresPlantillas'), 
+    path('DetalleDistribuidoresConPlantillas/', views.detalleDistribuidores, name = 'detalleDistribuidoresPlantillas'),
+
+    path('listadoEstancosConPlantillas/', views.listaEstancos, name = 'listaEstancosPlantillas'), 
+    path('DetalleEstancosConPlantillas/', views.detalleEstancos, name = 'detalleEstancosPlantillas'),
+
 ]
