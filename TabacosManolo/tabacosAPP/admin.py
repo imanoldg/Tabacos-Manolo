6e4a13d9eea7a32df21b5f6da4f6e8ff2f9ec17b
Imanol Domínguez <ima.dominguez@opendeusto.es>
2024-12-05 10:06:38 +0100

    json-ld añadido a agendavue

--- TabacosManolo/tabacosAPP/admin.py
+++ TabacosManolo/tabacosAPP/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Distribuidor, Estanco, Cliente, Marca, Cigarrillo, Puro
 
+admin.site.site_header = 'Tabacos Manolo Admin'
+
 # Register your models here.
 admin.site.register(Distribuidor)
 admin.site.register(Estanco)
